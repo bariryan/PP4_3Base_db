555a8d2eaab3b3d27d0d244887487d05fa3cb73b
Barry Ryan <bariryan@gmail.com>
2022-11-01 12:13:59 +0000

    the application can now serve static html with images for the database

--- threebase/models.py
+++ threebase/models.py
@@ -3,4 +3,4 @@ from django.db import models
 
 class Movie(models.Model):
     name = models.CharField(max_length=200)
-    image = models.ImageField(upload_to='files/covers')
+    image = models.ImageField(upload_to='threebase/files/covers')
--- threebase/settings.py
+++ threebase/settings.py
@@ -12,8 +12,12 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
+from django.urls import URLPattern
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+MEDIA_ROOT = ''
+MEDIA_URL = ''
 
 
 # Quick-start development settings - unsuitable for production
